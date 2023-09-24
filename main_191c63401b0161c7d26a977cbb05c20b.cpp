8f1f98c62de32570674eae2540e3b02a6ccd5999
dominikdzikas <144335187+dominikdzikas@users.noreply.github.com>
2023-09-24 21:34:39 +0200

    Hibák kommentelése

--- main_191c63401b0161c7d26a977cbb05c20b.cpp
+++ main_191c63401b0161c7d26a977cbb05c20b.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //NELEMENTS helyett N_ELEMEMTS
+    std::cout << '1-100 ertekek duplazasa' // pontos vesszo es std::endl hianya
+    for (int i = 0;) //hianyos for ciklus
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //rosszul haszanlt for ciklus
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // kiirando ertekek hainya, std::endl es pontosvesszo hianya
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] //hianyzik a pontosvesszo
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
